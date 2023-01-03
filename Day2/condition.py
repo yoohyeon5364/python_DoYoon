9d2a40e27dcda2e2a9cd4fa047a338cee11deaad
yoohyeon5364 <tvgguldeocki@gmail.com>
2023-01-03 19:47:07 +0900

    condition.py

--- Day2/condition.py
+++ Day2/condition.py
@@ -95,3 +95,12 @@
 #     if i % 5 == 0:
 #         sum *= i
 # print(sum)
+
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(sum)
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 4 == 0 and i % 5 == 0:
+        print(i)
