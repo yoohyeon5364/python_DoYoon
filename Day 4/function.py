a7de5b84dee10058cd194f94a93193367259ad66
yoohyeon5364 <tvgguldeocki@gmail.com>
2023-01-10 20:56:53 +0900

    함수

---
+++ Day 4/function.py
@@ -0,0 +1,99 @@
+"""
+함수
+javascript: function
+python: def 함수 이름 ():
+            실행 할 코드 작성
+.함수는 작성하고, 불러 줘야 함
+ex1) 
+def 이윤성():
+    print("나는 이윤성")
+
+이윤성()
+이윤성()
+
+def 함수 이름 (입력):
+            실행 할 코드 작성
+
+ex2) 
+def 김민기(숫자):
+    for i in range(숫자):
+        print("나는 김민기")
+
+김민기(3)
+
+def 함수 이름 (입력):
+    실행 할 코드 작성
+    return 출력
+
+def 김도윤(숫자):
+    for i in range(숫자):
+        print("나는 김도윤")
+    return i
+
+print(김도윤(3))
+"""
+
+
+# def 이윤성():
+#     print("나는 이윤성")
+
+
+# 이윤성()
+# 이윤성()
+
+
+# def 김민기():
+#     for i in range():
+#         print("나는 김민기")
+
+
+# 김민기(3)
+
+
+# def 김도윤(숫자):
+#     for i in range(숫자):
+#         print("나는 김도윤")
+#     return i
+
+
+# print(김도윤(3))
+
+# Q1. 더하기
+# def 더하기(x + y):
+#     return x + y
+
+# print(더하기(4,6))
+# print(더하기(2,3))
+
+# Q2. 삼각형 넓이
+# def 삼각형넓이(x, y):
+#     return x * y / 2
+
+
+# print(삼각형넓이(2, 4))
+# print(삼각형넓이(10, 20))
+
+# Q3. 구구단
+# def 구구단(x):
+#     for i in range(1, 10):
+#         print(x * i)
+
+
+# 구구단(3)
+# 구구단(7)
+
+# Q4. 제곱수
+# def 제곱수(x):
+#     return x * x
+
+
+# print(제곱수(5))
+# print(제곱수(10))
+
+# Q5. 정겉넓
+# def 정겉넓(x):
+#     return x * x * 6
+
+
+# print(정겉넓(2))
+# print(정겉넓(3))
